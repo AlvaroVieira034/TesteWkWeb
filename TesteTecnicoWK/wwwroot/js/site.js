4b77b9720af0d6a5b1a6cdd019fece9819323f10
AlvaroVieira034 <amvalvaro@hotmail.com>
2025-02-01 00:26:14 -0300

    Conclusão integração API viacep cadastro de clientes paginação, ordenação e filtros dos grids Ajustes wwwroot/js/site.js (paginação do grid)

--- TesteTecnicoWK/wwwroot/js/site.js
+++ TesteTecnicoWK/wwwroot/js/site.js
@@ -3,6 +3,39 @@
 
 // Write your JavaScript code.
 
+
+$(document).ready(function () {
+    $('#table-grid').DataTable({
+        "ordering": true,
+        "paging": true,
+        "searching": true,
+        "oLanguage": {
+            "sEmptyTable": "Nenhum registro encontrado na tabela",
+            "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+            "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Mostrar _MENU_ registros por página",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Próximo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Último"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+        }
+    });
+});
+
+
 setTimeout(function () {
 	$(".alert").fadeOut("slow", function () {
 		$(this).alert("close");
